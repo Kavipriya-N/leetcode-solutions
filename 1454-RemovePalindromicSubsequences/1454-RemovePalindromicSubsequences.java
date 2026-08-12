760f57cd7a9e7673fca7e4fcde125cb75c9848ba
Kavipriya N <kavipriyanachimuthu@gmail.com>
2026-08-12 16:22:36 +0530

    Create 1454-RemovePalindromicSubsequences

---
+++ 1454-RemovePalindromicSubsequences/1454-RemovePalindromicSubsequences.java
@@ -0,0 +1,20 @@
+// Last updated: 12/08/2026, 16:22:35
+class Solution {
+    public int removePalindromeSub(String s) {
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while(left < right) {
+
+            if(s.charAt(left) != s.charAt(right)) {
+                return 2;
+            }
+
+            left++;
+            right--;
+        }
+
+        return 1;
+    }
+}
