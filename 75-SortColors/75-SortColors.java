82e930c46087ad8cc687228f71e7046e86a00f38
Kavipriya N <kavipriyanachimuthu@gmail.com>
2026-08-12 16:28:34 +0530

    Create 75-SortColors

---
+++ 75-SortColors/75-SortColors.java
@@ -0,0 +1,20 @@
+// Last updated: 12/08/2026, 16:28:33
+
+class Solution {
+    public void sortColors(int[] arr) {
+        
+        int n=arr.length;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n-i-1;j++)
+            {
+                if(arr[j]> arr[j+1])
+                {
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+}
