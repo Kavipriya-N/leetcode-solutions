775a9879e808dd00557031bc133796bb4bdfb4bc
Kavipriya N <kavipriyanachimuthu@gmail.com>
2026-08-12 16:26:49 +0530

    Create 202-HappyNumber

---
+++ 202-HappyNumber/202-HappyNumber.java
@@ -0,0 +1,20 @@
+// Last updated: 12/08/2026, 16:26:48
+class Solution {
+    public boolean isHappy(int n) {
+
+        while (n != 1 && n != 4) {
+
+            int sum = 0;
+
+            while (n > 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
+            }
+
+            n = sum;
+        }
+
+        return n == 1;
+    }
+}
