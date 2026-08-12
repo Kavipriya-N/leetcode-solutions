3ace3ce65969637991bc84a7a377672209e61af0
Kavipriya N <kavipriyanachimuthu@gmail.com>
2026-08-12 16:22:54 +0530

    Create 1260-DayOfTheYear

---
+++ 1260-DayOfTheYear/1260-DayOfTheYear.java
@@ -0,0 +1,25 @@
+// Last updated: 12/08/2026, 16:22:53
+class Solution {
+    public int dayOfYear(String date) {
+
+        int[] days = {31,28,31,30,31,30,31,31,30,31,30,31};
+
+        String[] d = date.split("-");
+
+        int year = Integer.parseInt(d[0]);
+        int month = Integer.parseInt(d[1]);
+        int day = Integer.parseInt(d[2]);
+
+        if((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+            days[1] = 29;
+        }
+
+        int total = day;
+
+        for(int i = 0; i < month - 1; i++) {
+            total += days[i];
+        }
+
+        return total;
+    }
+}
