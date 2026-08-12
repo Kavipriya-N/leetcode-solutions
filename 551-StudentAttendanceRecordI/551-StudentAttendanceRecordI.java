4c0ec134a38eae6788c5850adaec0cea1a6a6c46
Kavipriya N <kavipriyanachimuthu@gmail.com>
2026-08-12 16:24:39 +0530

    Create 551-StudentAttendanceRecordI

---
+++ 551-StudentAttendanceRecordI/551-StudentAttendanceRecordI.java
@@ -0,0 +1,32 @@
+// Last updated: 12/08/2026, 16:24:38
+class Solution {
+    public boolean checkRecord(String s) {
+
+        int absent = 0;
+        int late = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) == 'A') {
+                absent++;
+                late = 0;
+            }
+            else if (s.charAt(i) == 'L') {
+                late++;
+
+                if (late == 3) {
+                    return false;
+                }
+            }
+            else {
+                late = 0;
+            }
+
+            if (absent >= 2) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
