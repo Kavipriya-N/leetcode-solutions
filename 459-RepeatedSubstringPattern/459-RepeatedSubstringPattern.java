fb080f09a31229427b320541c264a2b17884037d
Kavipriya N <kavipriyanachimuthu@gmail.com>
2026-08-12 16:24:56 +0530

    Create 459-RepeatedSubstringPattern

---
+++ 459-RepeatedSubstringPattern/459-RepeatedSubstringPattern.java
@@ -0,0 +1,26 @@
+// Last updated: 12/08/2026, 16:24:55
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+
+        int n = s.length();
+
+        for(int len = 1; len <= n / 2; len++) {
+
+            if(n % len == 0) {
+
+                String sub = s.substring(0, len);
+                String temp = "";
+
+                for(int i = 0; i < n / len; i++) {
+                    temp += sub;
+                }
+
+                if(temp.equals(s)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
