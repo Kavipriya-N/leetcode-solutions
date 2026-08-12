86293a093f07570e39643383aac8710fd71f5428
Kavipriya N <kavipriyanachimuthu@gmail.com>
2026-08-12 16:24:30 +0530

    Create 575-DistributeCandies

---
+++ 575-DistributeCandies/575-DistributeCandies.java
@@ -0,0 +1,13 @@
+// Last updated: 12/08/2026, 16:24:29
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        int n=candyType.length;
+        int caneat=n/2;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<n;i++){
+
+            map.put(candyType[i],map.getOrDefault(candyType[i],0)+1);
+        }
+        return Math.min(caneat,map.size());
+    }
+}
